47071826ee0c84b7ccac88cd4ee96c1200aa395b
Davi Moreira Campos <12116901@aluno.newtonpaiva.br>
2024-10-04 20:21:05 -0300

    first commit

---
+++ src/main/java/com/av/first/controllers/FipeService.java
@@ -0,0 +1,10 @@
+package com.av.first.controllers;
+
+public class FipeService {
+
+    public String consultarMarcas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'consultarMarcas'");
+    }
+
+}
